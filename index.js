8972c334e09630fe133b8d5e1c52bed5efaca78a
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-10-28 01:10:56 +0530

    adddddd

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ import express from 'express';
 import Hello from './Controllers/GlobalControllers.js';
 import Add from './Controllers/Addition.js';
 import router from './Routes/index.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
 
 const app = express();
-app.get('/', function (req, res) {
-    res.send('Hello Rahul')
+
+dotenv.config();
+app.use(morgan('dev'))
+
+app.use((req, res, next)=> {
+    // res.send('Hello Rahul')
+    console.log('hi from middleware use')
+    next();
 })
 
-app.get('/controllers', Hello)
+app.use('/controllers', Hello)
 
-app.get('/addition', Add)
+app.use('/addition', Add)
 
 app.use('/app/v1', router)
+
+
+mongoose.connect(process.env.MONGO_URL).then(()=> console.log('database created'))
 
 app.listen(8000, () => console.log("App is running on port 8000 go and check"))
